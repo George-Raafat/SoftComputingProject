13870f6b3dab70bc81f2eb05d2c68a75b47de972
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2025-10-23 16:42:25 +0300

    Fixed SteadyStateReplacement (Again)
    Made the selected k elements randomized instead of selecting the first k elements

--- src/genetic_algorithms/replacement/SteadyStateReplacement.java
+++ src/genetic_algorithms/replacement/SteadyStateReplacement.java
@@ -2,11 +2,14 @@ package genetic_algorithms.replacement;
 
 import genetic_algorithms.Chromosome;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class SteadyStateReplacement<ChomoT extends Chromosome<ChomoT, ?>> implements ReplacementStrategy<ChomoT> {
 
     private final int k;
+    private final Random random = new Random();
 
     public SteadyStateReplacement(int k) {
         this.k = k;
@@ -16,8 +19,15 @@ public class SteadyStateReplacement<ChomoT extends Chromosome<ChomoT, ?>> implem
     public List<ChomoT> replace(List<ChomoT> oldPopulation, List<ChomoT> offspring) {
         List<ChomoT> newGeneration = new ArrayList<>(oldPopulation);
 
-        for (int i = 0; i < k && i < offspring.size() && i < newGeneration.size(); ++i) {
-            newGeneration.set(i, offspring.get(i));
+        List<Integer> indices = new ArrayList<>();
+        for (int i = 0; i < oldPopulation.size(); ++i) {
+            indices.add(i);
+        }
+        Collections.shuffle(indices, random);
+
+        for (int i = 0; i < k && i < offspring.size() && i < indices.size(); ++i) {
+            int replaceIndex = indices.get(i);
+            newGeneration.set(replaceIndex, offspring.get(i));
         }
 
         return newGeneration;
