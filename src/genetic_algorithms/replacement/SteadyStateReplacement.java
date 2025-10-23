36c8bb1d43e0d057a50c645e6cb58a4c84a60990
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2025-10-23 16:29:22 +0300

    Fixed SteadyStateReplacement

--- src/genetic_algorithms/replacement/SteadyStateReplacement.java
+++ src/genetic_algorithms/replacement/SteadyStateReplacement.java
@@ -1,29 +1,23 @@
 package genetic_algorithms.replacement;
 
 import genetic_algorithms.Chromosome;
-
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class SteadyStateReplacement<ChomoT extends Chromosome<ChomoT, ?>> implements ReplacementStrategy<ChomoT> {
 
-    public SteadyStateReplacement() {}
+    private final int k;
+
+    public SteadyStateReplacement(int k) {
+        this.k = k;
+    }
 
     @Override
     public List<ChomoT> replace(List<ChomoT> oldPopulation, List<ChomoT> offspring) {
+        List<ChomoT> newGeneration = new ArrayList<>(oldPopulation);
 
-        int populationSize = oldPopulation.size();
-
-        List<ChomoT> combinedPopulation = new ArrayList<>(oldPopulation);
-        combinedPopulation.addAll(offspring);
-
-
-        combinedPopulation.sort((a, b) -> Double.compare(b.getFitness(), a.getFitness()));
-
-        List<ChomoT> newGeneration = new ArrayList<>();
-        for (int i = 0; i < populationSize && i < combinedPopulation.size(); i++) {
-            newGeneration.add(combinedPopulation.get(i));
+        for (int i = 0; i < k && i < offspring.size() && i < newGeneration.size(); ++i) {
+            newGeneration.set(i, offspring.get(i));
         }
 
         return newGeneration;
