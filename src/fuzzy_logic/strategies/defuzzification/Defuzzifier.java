f6aa67a8b0aee819072a36fa579e87c48c382253
ahmed-alaa-22 <147052378+ahmed-alaa-22@users.noreply.github.com>
2025-11-26 19:42:18 +0200

    Update Defuzzifier.java

--- src/fuzzy_logic/strategies/defuzzification/Defuzzifier.java
+++ src/fuzzy_logic/strategies/defuzzification/Defuzzifier.java
@@ -1,4 +1,15 @@
 package fuzzy_logic.strategies.defuzzification;
 
+import fuzzy_logic.model.LinguisticVariable;
+import fuzzy_logic.strategies.membership.MembershipFunction;
+
+import java.util.List;
+import java.util.Map;
+
 public interface Defuzzifier {
+
+    double defuzzify(
+            LinguisticVariable outputVariable,
+            Map<String, List<Double>> activations
+    );
 }
